8ba060d0890cd3ac6cd4501380406b405ca12f2a
Vasko <vasko5petrov@gmail.com>
2019-03-14 14:18:54 +0200

    User model setPassword()

--- models/user.js
+++ models/user.js
@@ -39,12 +39,22 @@ UserSchema.plugin(uniqueValidator, { message: "This {PATH} is already taken"});
 const User = module.exports = mongoose.model('User', UserSchema);
 
 module.exports.addUser = (newUser, callback) => {
-    // Crypt the password and save the user
-    bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if(err) callback({message: 'Failed to register user'}, null);
+    User.setPassword(newUser.password)
+        .then(hash => {
             newUser.password = hash;
             newUser.save(callback);
+        })
+        .catch(err => callback(err, null));
+}
+
+module.exports.setPassword = (password) => {
+    return new Promise((resolve, reject) => {
+        bcrypt.genSalt(10, (err, salt) => {
+            if(err) reject({message: 'Failed to register user'});
+            bcrypt.hash(password, salt, (err, hash) => {
+                if(err) reject({message: 'Failed to register user'});
+                resolve(hash);
+            });
         });
     });
 }
